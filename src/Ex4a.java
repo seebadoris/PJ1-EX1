cb6dbe5f864966f275072d5c26dd21ba0fa3485a
seebadoris <seebadoris666@gmail.com>
2017-10-11 13:21:44 +0200

    decimal

--- src/Ex4a.java
+++ src/Ex4a.java
@@ -19,8 +19,8 @@ public class Ex4a {
 		dblMNet=dblMBrut*(intTVA/100)+dblMBrut;			//si on met la parenthese et que int est divise en 100 cela donne "0" pas double :))
 		dblMNet=dblMBrut*intTVA/100+dblMBrut;			//ici il fait le brut *21 et ensuite divise en 100 avec la variable double 
 														//correct
-		
-		System.out.println("Pour le prix brut de "+dblMBrut+" €, le prix net est de "+ DfFormat1.format(dblMNet)+" €.");
+		System.out.println("Pour le prix brut de "+dblMBrut+" €, le prix net est de "+ dblMNet+" €.");
+		System.out.println("Pour le prix brut de "+dblMBrut+" €, le prix net est de "+ DfFormat1.format(dblMNet)+" €.");		//avec 2décimal
 	}
 
 }
