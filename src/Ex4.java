e57561569546448ebcf0d14abaa496253e898437
seebadoris <seebadoris666@gmail.com>
2017-10-11 12:41:39 +0200

    Decimal Ex4

--- src/Ex4.java
+++ src/Ex4.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 
 public class Ex4 {
 
@@ -7,13 +8,17 @@ public class Ex4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		DecimalFormat DdFormat=new DecimalFormat("#.##");
+		
 		double dblMBrut,dblMNet,dblTVA;
 
-		dblMBrut=20;
+		dblMBrut=20.10;
 		dblTVA=1.21;
 		dblMNet=dblMBrut*dblTVA;
 		
 		System.out.println("Pour le prix brut de "+dblMBrut+" €, le prix net est de "+ dblMNet+" €.");
+		
+		System.out.println("Pour le prix brut de "+dblMBrut+" €, le prix net est de "+ DdFormat.format(dblMNet)+" €. Avec le format de 2 décimal.");
 	}
 
 }
