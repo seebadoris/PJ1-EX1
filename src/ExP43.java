c11a913b5aebd37852193dce24d7a306e9a74d2c
seebadoris <seebadoris666@gmail.com>
2017-10-11 13:36:46 +0200

    Conversions explicites P43

---
+++ src/ExP43.java
@@ -0,0 +1,24 @@
+
+public class ExP43 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short shMaVariableShort; 
+		int intMaVariableInt; 
+		double dblMaVariableDouble; 
+		 
+
+		intMaVariableInt = 156000;		//en binaire 100110000101100000 sachant que le int peut avoir 32bits
+		dblMaVariableDouble=3.5; 
+		
+		shMaVariableShort = (short)intMaVariableInt;	//Le short ne peut contenir que 16bits donc de l'int precedent (100110000101100000)
+														//on ne peut prendre que les 16premieres bits (0110000101100000)
+		
+		intMaVariableInt = (int)dblMaVariableDouble; 	//La variable int ne prend que des donnée entiere pas de decimal d'ou le 3
+		 
+		System.out.println(shMaVariableShort); 
+		System.out.println(intMaVariableInt); 
+
+	}
+
+}
